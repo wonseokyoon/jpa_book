f52144c01486628cca313cffa7337d46c683f40a
wonseokyoon <01088115030a@gmail.com>
2025-02-07 12:52:58 +0900

    first commit

---
+++ src/test/java/ORM/JpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
